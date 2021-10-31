402c82ccbb693239bba017805492e38a90ae52f1
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-31 14:16:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie2Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Вычисление числа Пи:");
+            Console.WriteLine("Введите кол-во слагаемых");
+            int Count = int.Parse(Console.ReadLine());
+            double n = 3;
+            double p = 1;           
+            while (i < Count)
+            {
+                p = p - (1 / n) + (1 / (n + 2));
+                n = n + 4;
+                i++;
+            }
+            Console.WriteLine(p * 4);
+
+        }
+    } 
+    
+}
